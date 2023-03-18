0c7b950a0a9355a69eb894e4c74b4c700f0b0da4
BBlyde <benjaminblondey@orange.fr>
2023-03-18 20:17:53 +0100

    routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MealListComponent } from './component/meal-list/meal-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'meals', component: MealListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
